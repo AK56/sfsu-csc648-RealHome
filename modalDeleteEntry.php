ccb0bcbd117a970a5e86fded12a02757fa148d7e
Gurpartap S Gill <gillgur@mail.sfsu.edu>
2015-06-02 01:44:34 -0700

    Create modalDeleteEntry.php
    To create new entry in the database.

---
+++ modalDeleteEntry.php
@@ -0,0 +1,38 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == "GET") {
+    $checkboxID = $_GET['$checkboxID'];
+};
+?>
+
+<!DOCTYPE html>
+ <div class="modal fade bs-example-modal-sm" id="ModalDeleteEntry" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" >
+    <div class="modal-dialog modal-sm">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+          <h4 class="modal-title">Delete Entry</h4>
+        </div>
+        <div class="modal-body">
+		<div class="alert alert-danger" role="alert">
+                    <?php echo $checkboxID;
+                    ?>Warning! Deleting an Entry will permanently remove all its contents and data, Enter the ID of the Entry you wish to delete.</div>
+		<form class="form-editPage" role="form" enctype=multipart/form-data action="Includes/Process-CreateEntry.php" method="POST">
+                                <div class="form-group">
+                                    <label for="InputEntryToDelete">ID: </label>
+                                    <input type="text" class="form-control" name="InputEntryToDelete" placeholder="Enter ID">
+                                </div>
+                          
+
+
+                                <button class="btn btn-lg btn-primary" type="submit">Delete!!!! </button>
+                            </form>
+					
+
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+         
+        </div>
+      </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+  </div>

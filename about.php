600811bd23a758dd21283d548e64d2acd78c2583
AK56 <ggurpartap@yahoo.com>
2015-06-08 02:08:44 -0700

    added about/contact pages

---
+++ about.php
@@ -0,0 +1,131 @@
+<?php
+session_start();
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>About RealHome</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="dist/css/bootstrap-table.min.css" rel="stylesheet">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>   
+  <script type="text/javascript" language="javascript" charset="utf-8" src="dist/js/bootstrap.min.js"></script>
+  <script type="text/javascript" language="javascript" charset="utf-8" src="dist/js/bootstrap-table.min.js"></script>
+
+   <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel='stylesheet' type='text/css'>
+
+   <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+
+  </head>
+
+  <body>
+   
+    
+  
+  <?php include('Includes/navBar.html'); ?>
+
+<!-- include modal -->
+<?php include('Includes/modals/includeModals.php'); ?>
+  
+    <div class="container-fluid"style="background-color: #f8efc0">
+
+      <div class="row row-offcanvas row-offcanvas-right">
+
+        <div class="col-xs-12 col-md-12 col-lg-12"style="background-color: #f8efc0">
+          <p class="pull-right visible-xs">
+            <button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas">Toggle nav</button>
+          </p>
+          
+          <div id="jumbo-top" class="jumbotron"style="background-color: #f8efc0">
+		  <div height="5%" width="100%"style="margin-left:30%;">		 
+		  	<br>
+                            <p><img src="images/realhomelogo.png"  height="25%" width="25%"  alt="Image1" ><br><sub style="font-size: 30px; margin-left: 10%">Find Your Real Home here</sub></p>
+                        </div>
+              <br>
+	<!--/*RealHome logo uploaded and now take care of search label and form*/ -->
+                
+          
+          </div>
+         
+          <div class="row">
+
+              <pre><h3>The RealHome Story</h3>
+
+<b>Innovation meets Influence</b>
+ 
+Before Silicon Valley was duly named, its history as the hotbed of innovation was well noted. Our catchy moniker reflects the giants of technology 
+
+who line the corridors of Cupertino, Mountain View, Menlo Park, Palo Alto, and Sunnyvale, the wave of progressive firms that have shaped electronics, 
+
+e-commerce and social interaction all call this place home.
+ 
+
+In its truest form, innovation knows no boundaries. It isn’t contained by market or economic trends, it is the consequence of bursting out of great
+
+ideas. That is the sort of idea we have behind RealHome, the innovative idea of having real estate services on the web for the Users all around the 
+
+globe to benefit from it. RealHome is a startup in the industry and is fully committed to establish itself in the Real Estate Market as a online Web 
+
+Application. Its goals are towards its users be it Customers out there looking to buy homes or Realtors, who looking for an innovative way to sell off 
+
+properties in their respective zones. 
+
+
+RealHome is the route both these users will take to connect and work with each other. RealHome is loaded with services for the home buyers to make their 
+
+home buying experience by starting them off with something great, which cannot come anywhere else besides the Home screen of RealHome. 
+
+
+RealHome want its users to feel a sense of appreciation for the time they will input by providing honest information about the Real Estate Market and a 
+
+great User friendly interface for the great process of home buying in one's life.
+
+
+So come Join us and feel like you are in home while you find your way home !
+        </pre>
+        
+            </div><!--/span-->
+            
+          </div><!--/row-->
+        </div><!--/span-->
+
+       
+      <!--/row-->
+
+      <hr>
+
+      <nav class="nav navbar navbar-inverse navbar-fixed-bottom" role="navigation">
+    <div class="container-fluid" style="margin: 0px 0px 0px 0px; padding: 0px 0px 0px 0px;">
+        <div class="navbar-header">
+          
+            <h5 style=" text-align : center ; color: floralwhite" >&copy; SFSU/FAU/FULDA Software Engineering Project Fall, 2014. For Demonstration Only.</h5>
+        </div>
+    </div>
+</nav>
+
+
+    </div><!--/.container-->
+
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+   <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ contact.php
@@ -0,0 +1,108 @@
+<?php
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Contact RealHome</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="dist/css/bootstrap-table.min.css" rel="stylesheet">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>   
+  <script type="text/javascript" language="javascript" charset="utf-8" src="dist/js/bootstrap.min.js"></script>
+  <script type="text/javascript" language="javascript" charset="utf-8" src="dist/js/bootstrap-table.min.js"></script>
+
+   <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel='stylesheet' type='text/css'>
+
+   <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+
+  </head>
+
+  <body style="background-color: #f8efc0">
+   
+    
+     <!-- Modal Manage Account -->
+  <?php include('Includes/navBar.html'); ?>
+ <!-- /.modal -->
+	
+<!-- include modal -->
+<?php include('Includes/modals/includeModals.php'); ?>
+  
+    <div class="container-fluid" style="background-color: #f8efc0">
+
+      <div class="row row-offcanvas row-offcanvas-right">
+
+        <div class="col-xs-12 col-md-12 col-lg-12"style="background-color: #f8efc0">
+          <p class="pull-right visible-xs">
+            <button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas">Toggle nav</button>
+          </p>
+          
+          <div id="jumbo-top" class="jumbotron"style="background-color: #f8efc0">
+		  <div height="5%" width="100%"style="margin-left:30%;">		 
+		  	<br>
+                            <p><img src="images/realhomelogo.png"  height="25%" width="25%"  alt="Image1" ><br><sub style="font-size: 30px; margin-left: 10%">Find Your Real Home here</sub></p>
+                        </div>
+              <br>
+	<!--/*RealHome logo uploaded and now take care of search label and form*/ -->
+                
+          
+          </div>
+         
+   
+          
+          
+          
+            
+          </div><!--/row-->
+        </div><!--/span-->
+
+       
+      <!--/row-->
+
+      
+      <pre style='background:graytext'>
+        <h3>Contact our Team:</h3>
+        <b>RealHome Inc.</b>
+        10000 Galaxy Way
+        Milky Way, EA 100
+
+        Phone No. 1-800-800-8001
+        Email:    f14g21@sfsuswe.com
+        </pre>
+      
+      
+      <hr>
+<nav class="nav navbar navbar-inverse navbar-fixed-bottom" role="navigation">
+    <div class="container-fluid" style="margin: 0px 0px 0px 0px; padding: 0px 0px 0px 0px;">
+        <div class="navbar-header">
+          
+            <h5 style=" text-align : center ; color: floralwhite" >&copy; SFSU/FAU/FULDA Software Engineering Project Fall, 2014. For Demonstration Only.</h5>
+        </div>
+    </div>
+</nav>
+
+
+    </div><!--/.container-->
+
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+   <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+  </body>
+</html>

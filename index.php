3733f572ffa7af7c06a96bfef12f3ba8a47f816f
Gurpartap S Gill <gillgur@mail.sfsu.edu>
2015-06-07 18:34:04 -0700

    Home Page of RealHome

--- index.php
+++ index.php
@@ -260,8 +260,6 @@ mysqli_close($con);
 
     </div><!--/.container-->
 
-
-
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->

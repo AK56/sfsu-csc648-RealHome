479e317a16294492eaf76d7c7056c81c094fe418
Gurpartap S Gill <gillgur@mail.sfsu.edu>
2015-06-14 13:10:12 -0700

    Database connection check

---
+++ connect2SQL.php
@@ -0,0 +1,20 @@
+<?php
+
+function con2SQL() {
+    $con = mysqli_connect("sfsuswe.com", "f14g21", "21mgrs2014");
+    if (!$con) {
+        exit('Connect Error (' . mysqli_connect_errno() . ') '
+                . mysqli_connect_error());
+    }
+    //set the default client character set
+    mysqli_set_charset($con, 'utf-8');
+    mysqli_select_db($con, "student_f14g21");
+    
+    return $con;
+}
+
+function endCon2SQL($con){
+    //mysqli_kill($con); //needs 2 parameters
+    mysqli_close($con);
+}
+?>

9d616cbbeabc1899b403beba541504281e00cdda
Gurpartap S Gill <gillgur@mail.sfsu.edu>
2015-06-14 13:04:29 -0700

    Frequently Asked Q. Web page

---
+++ faq.php
@@ -0,0 +1,129 @@
+<?php
+session_start();
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>RealHome FAQ</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="dist/css/bootstrap-table.min.css" rel="stylesheet">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>   
+  <script type="text/javascript" language="javascript" charset="utf-8" src="dist/js/bootstrap.min.js"></script>
+  <script type="text/javascript" language="javascript" charset="utf-8" src="dist/js/bootstrap-table.min.js"></script>
+
+   <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel='stylesheet' type='text/css'>
+
+   <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+
+  </head>
+
+  <body>
+   
+  <?php include('Includes/navBar.html'); ?>
+
+	
+<!-- include modal -->
+<?php include('Includes/modals/includeModals.php'); ?>
+
+    <div class="container-fluid"style="background-color: #f8efc0">
+
+      <div class="row row-offcanvas row-offcanvas-right">
+
+        <div class="col-xs-12 col-md-12 col-lg-12"style="background-color: #f8efc0">
+          <p class="pull-right visible-xs">
+            <button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas">Toggle nav</button>
+          </p>
+          
+          <div id="jumbo-top" class="jumbotron"style="background-color: #f8efc0">
+		  <div height="5%" width="100%"style="margin-left:30%;">		 
+		  	<br>
+                            <p><img src="images/realhomelogo.png"  height="25%" width="25%"  alt="Image1" ><br><sub style="font-size: 30px; margin-left: 10%">Find Your Real Home here</sub></p>
+                        </div>
+              <br>
+	<!--/*RealHome logo uploaded and now take care of search label and form*/ -->
+                
+          
+          </div>
+         
+          <h3>Frequently Asked Questions:</h3>
+        <b>How does Registration works ?</b>
+        <br>
+        <pre>The registration process is quite smooth and it requires just your name and contact information. You can sign up via 
+your email or can use your facebook or Google credentials.</pre>            
+        
+        <b>What is the benefit of registering to RealHome ?</b><br>
+<pre>To use all the services RealHome provides, one has to register. The Registered customer can save any property as his favorite, 
+can also have a personal profile with information on kind of properties he/she interested in, and will receive email once anything 
+related to their interests comes up on RealHome. Also the registered customers can watch on any property and can as well have the 
+contact information of the Realtor associated with that property. All the data of Registered Customer is saved on RealHome.</pre>
+        
+        <b>Do I have to register to contact the Realtor?</b>
+        <br>
+<pre>Yes, as mentioned in the above answer. Only Registered Customers will be able to get Realtor's contact information.</pre>
+        
+        <b>What does "Save Favorite" means on the Property page ?</b>
+        <br>
+<pre>It means that if you really like any property, you can save it in your favorite list of properties. The list is saved for each 
+Registered Customer and you can see that property as long it is on RealHome anytime by logging into your RealHome profile.  </pre>
+        
+        <b>What does "Watch this Unit" means ?</b>
+        <br>
+<pre>It means that you can stay updated when anything regards to the property changes. Like you will be notified via email, if the 
+price, Property status or any essential information about the property changes, which you don't want to miss.</pre>
+        
+        <b>Is my Personal information shared or kept private ?</b>
+        <br>
+<pre>Only your Contact information is shared with the Realtor to whose property you showed interest in. Besides RealHome strictly 
+follows Member Privacy rules and committed to respect the privacy of its members.</pre>
+        
+        <b>How to I start Home Buying Process ?</b>
+        <br>
+<pre>Once you know the property you want to start process of, just contact the Realtor associated with property, and the member of our
+great team of Realtors will take care of you from there.</pre>
+ 
+ 
+            
+          </div><!--/row-->
+        </div><!--/span-->
+
+       
+      <!--/row-->
+
+      <hr>
+
+     <nav class="nav navbar navbar-inverse navbar-fixed-bottom" role="navigation">
+    <div class="container-fluid" style="margin: 0px 0px 0px 0px; padding: 0px 0px 0px 0px;">
+        <div class="navbar-header">
+          
+            <h5 style=" text-align : center ; color: floralwhite" >&copy; SFSU/FAU/FULDA Software Engineering Project Fall, 2014. For Demonstration Only.</h5>
+        </div>
+    </div>
+</nav>
+
+
+    </div><!--/.container-->
+
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+   <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+  </body>
+</html>
